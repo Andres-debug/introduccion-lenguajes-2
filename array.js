3e228a61f88ce8d029d7b69a38daef922b120754
Andres Felipe Diaz <andresf.diazm@unac.edu.co>
2025-03-10 18:43:42 -0500

    Metodos basicos array

---
+++ array.js
@@ -0,0 +1,13 @@
+let numeros = [10,20,30,50,85]
+
+console.log(numeros)
+numeros.push(78) // Se agrega al final del array
+console.log(numeros)
+numeros.unshift(9) // Se agrega al inicio del array
+console.log(numeros)
+numeros.pop() //Elimina el ultimo del array
+console.log(numeros)
+numeros.shift() // Elimina el primero del array
+console.log(numeros)
+numeros.splice(3,1) //Elimina por el indice
+console.log(numeros)
---
+++ arreglo.py
@@ -0,0 +1,13 @@
+numeros = [10,20,30,40,67,89]
+print(numeros)
+
+numeros.append(20) #Agrega al final del array
+print(numeros)
+numeros.insert(0,45) #Agrega al inicio del array
+print(numeros)
+numeros.pop() #Elimina el ultimo del array
+print(numeros)
+numeros.pop(0) #Elimina el primero
+print(numeros)
+del numeros[4] #Elimina por indice
+print(numeros)
