e1312b207a87870cc1517e4153b34f05dee902e1
Andres Felipe Diaz <andresf.diazm@unac.edu.co>
2025-03-10 18:55:09 -0500

    Filter y Ordenamiento

--- array.js
+++ array.js
@@ -1,13 +1,20 @@
-let numeros = [10,20,30,50,85]
+let numeros = [10,20,30,50,85,3,23,11,5]
 
-console.log(numeros)
-numeros.push(78) // Se agrega al final del array
-console.log(numeros)
-numeros.unshift(9) // Se agrega al inicio del array
-console.log(numeros)
-numeros.pop() //Elimina el ultimo del array
-console.log(numeros)
-numeros.shift() // Elimina el primero del array
-console.log(numeros)
-numeros.splice(3,1) //Elimina por el indice
-console.log(numeros)
+// console.log(numeros)
+// numeros.push(78) // Se agrega al final del array
+// console.log(numeros)
+// numeros.unshift(9) // Se agrega al inicio del array
+// console.log(numeros)
+// numeros.pop() //Elimina el ultimo del array
+// console.log(numeros)
+// numeros.shift() // Elimina el primero del array
+// console.log(numeros)
+// numeros.splice(3,1) //Elimina por el indice
+// console.log(numeros)
+
+// numeros.sort((a,b)=>a-b) //Ordena ASC
+// console.log(numeros)
+// numeros.sort((a,b)=>a-b).reverse() //Ordena DESC
+
+// let mayoresDe10 = numeros.filter(num=> num>10) // Metodo filtrar
+// console.log(mayoresDe10)
--- arreglo.py
+++ arreglo.py
@@ -1,13 +1,20 @@
-numeros = [10,20,30,40,67,89]
-print(numeros)
+numeros = [10,20,30,40,67,89,8,5,22]
+# print(numeros)
 
-numeros.append(20) #Agrega al final del array
+# numeros.append(20) #Agrega al final del array
+# print(numeros)
+# numeros.insert(0,45) #Agrega al inicio del array
+# print(numeros)
+# numeros.pop() #Elimina el ultimo del array
+# print(numeros)
+# numeros.pop(0) #Elimina el primero
+# print(numeros)
+# del numeros[4] #Elimina por indice
+# print(numeros)
+
+numeros.sort() #Orden ASC
 print(numeros)
-numeros.insert(0,45) #Agrega al inicio del array
-print(numeros)
-numeros.pop() #Elimina el ultimo del array
-print(numeros)
-numeros.pop(0) #Elimina el primero
-print(numeros)
-del numeros[4] #Elimina por indice
-print(numeros)
+numeros.sort(reverse=True) #Orden DESC
+
+mayores_de_10 = [num for num in numeros if num >10] #filter
+print(mayores_de_10)
