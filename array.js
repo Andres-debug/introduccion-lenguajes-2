89ad0a35ee677be063c7a22a797973927f5c05fb
Andres Felipe Diaz <andresf.diazm@unac.edu.co>
2025-03-10 19:25:00 -0500

    Inidice y Find

--- array.js
+++ array.js
@@ -19,9 +19,18 @@ let numeros = [10,20,30,50,85,3,23,11,5]
 // let mayoresDe10 = numeros.filter(num=> num>10) // Metodo filtrar
 // console.log(mayoresDe10)
 
-let dobles = numeros.map(num => num*2) //Transforma los datos y los multiplica por 2
-console.log(dobles)
+// let dobles = numeros.map(num => num*2) //Transforma los datos y los multiplica por 2
+// console.log(dobles)
 
-let suma = numeros.reduce((total,num)=> total +num,0)// Acumula valores
-//total es la variable que va acumular los valores y num la variable iteradora
-console.log(suma)
+// let suma = numeros.reduce((total,num)=> total +num,0)// Acumula valores
+// //total es la variable que va acumular los valores y num la variable iteradora
+// console.log(suma)
+
+console.log(numeros.includes(10)) //Valida si el elemento existe en el array
+console.log(numeros.indexOf(30)) //Devuelve el inidice donde se encuentra el elemento
+
+let encontrado = numeros.find(num=> num===30) //Encuentra el numero 30
+console.log(encontrado)
+let nombres=["Luis","Jose","Ana"]
+let encontrado2 = nombres.find(nombre=>nombre.startsWith("A"))//Encuentra el primero que empiece por A
+console.log(encontrado2)
--- arreglo.py
+++ arreglo.py
@@ -19,8 +19,17 @@ numeros = [10,20,30,40,67,89,8,5,22]
 # mayores_de_10 = [num for num in numeros if num >10] #filter
 # print(mayores_de_10)
 
-dobles = [num *2 for num in numeros] #Transforma los datos y lso multiplica por 2
-print(dobles)
+# dobles = [num *2 for num in numeros] #Transforma los datos y lso multiplica por 2
+# print(dobles)
 
-suma = sum(numeros)
-print(suma)
+# suma = sum(numeros)
+# print(suma)
+
+nombres = ["Juan","Alberto","Ana"]
+
+# print("Pedro" in nombres) #Validar si existe en el array
+
+# print (nombres.index("Juan")) #Indica el indice donde esta el elemento
+
+# encontrado = next((nombre for nombre in nombres if nombre.startswith("A")))
+# print (encontrado)
